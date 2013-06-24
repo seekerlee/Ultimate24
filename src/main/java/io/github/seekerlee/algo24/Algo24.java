cf6d8bc43f4414ff52d5fdbc8fd42c53fc501f70
seekerlee <lishi1608@gmail.com>
2013-06-24 17:51:42 +0800

    a change

--- src/main/java/io/github/seekerlee/algo24/Algo24.java
+++ src/main/java/io/github/seekerlee/algo24/Algo24.java
@@ -1,10 +1,24 @@
 package io.github.seekerlee.algo24;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * User: seeker
  * Date: 13-6-23
  * Time: 上午9:59
  */
 public class Algo24 {
+    public static Set<FractionalIntTrackable> algo24(List<FractionalIntTrackable> input) {
+        Set<FractionalIntTrackable> result = new HashSet<FractionalIntTrackable>();
 
+        return result;
+    }
+
+    private static void doit(List<FractionalIntTrackable> input, Set<FractionalIntTrackable> output) {
+        // TODO: check input
+        // end of recursion
+
+    }
 }
